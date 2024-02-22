25fbae6068f65667d172b97ef1dbdae33c9b2652
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-22 13:37:15 -0800

    Create Framed-reflection.js

---
+++ Framed-reflection.js
@@ -0,0 +1,14 @@
+/*
+You are given a message (text) that you choose to read in a mirror. 
+Return what you would see, complete with the mirror frame. 
+
+Example:
+  'Hello World'
+  would give:
+    *********
+    * olleH *
+    * dlroW *
+    *********
+
+Words in your solution should be left-aligned.
+*/
